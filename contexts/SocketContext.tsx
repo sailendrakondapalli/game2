8865891cb919ddda8774f9b53b7fe8cd7a346278
sailendrakondapalli <127012315+sailendrakondapalli@users.noreply.github.com>
2026-01-11 10:43:06 +0530

    Updated SocketContext.tsx

--- contexts/SocketContext.tsx
+++ contexts/SocketContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const GAME_SERVER_URL =
+const GAME_SERVER_URL = 
   process.env.EXPO_PUBLIC_GAME_SERVER_URL ||
   (typeof window !== 'undefined'
     ? `${window.location.protocol}//${window.location.hostname}:3001`
